6748fd2f761e1e9460a831419945c282367d6254
Hector Quiros Perez <hectorquirosperez@gmail.com>
2025-10-28 13:41:48 +0100

    Update 011_CÁLCULOS.sql

--- 011_CÁLCULOS.sql
+++ 011_CÁLCULOS.sql
@@ -1,4 +1,5 @@
 ALTER TABLE `ejercicioempresa`.`clientes` 
-ADD COLUMN `height` INT NULL AFTER `tipocliente`,
-ADD COLUMN `width` INT NULL AFTER `height`,
+ADD COLUMN `height` INT NOT NULL AFTER `tipocliente`,
+ADD COLUMN `width` INT NOT NULL AFTER `height`,
 ADD COLUMN `area` INT GENERATED ALWAYS AS (height * width) VIRTUAL AFTER `width`;
+--NO USAR VALORES NULOS--
